8fca085b553a0297521b3f1531e8e3620291c439
84709 <847073906@qq.com>
2019-08-21 21:53:17 +0800

    init project.

---
+++ src/store.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    count: 0,
+  },
+  mutations: {
+    increase() {
+      // eslint-disable-next-line no-plusplus
+      this.state.count++;
+    },
+  },
+  actions: {
+
+  },
+});
---
+++ src/views/Info.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    hello info component
+    <button type="button" v-on:click="add()">添加</button>
+  </div>
+</template>
+
+<script>
+import store from '@/store';
+
+export default {
+  name: 'Info',
+  store,
+  data() {
+    return {
+      msg: 'Hello vue',
+    };
+  },
+  mounted() {
+    window.vue = this;
+  },
+  methods: {
+    add() {
+      console.log('add Event from info!');
+      store.commit('increase');
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
